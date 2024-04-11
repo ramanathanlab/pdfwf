ebe7e6727a635643c8797e19b3125b5ae8c487ec
Kyle Hippe <kyle.hippe@gmail.com>
2024-04-10 22:05:06 -0500

    Starting test for parsl

--- pdfwf/parsl.py
+++ pdfwf/parsl.py
@@ -11,6 +11,8 @@ except ImportError:
 
 from typing import Sequence
 from typing import Union
+import os
+from subprocess import Popen
 
 from parsl.addresses import address_by_interface
 from parsl.config import Config
@@ -226,8 +228,79 @@ class PolarisSettings(BaseComputeSettings):
         return config
 
 
+class SunspotSettings(BaseComputeSettings):
+    """Configuration for running on Sunspot
+
+    Each GPU tasks uses a single tile"""
+
+    torch_device = 'xpu'
+    lammps_cmd = ('/home/knight/lammps-git/src/lmp_aurora_gpu-lward '
+                  '-pk gpu 1 -sf gpu').split()
+    lammps_env = {'OMP_NUM_THREADS': '1'}
+    cpus_per_node = 208
+    gpus_per_node = 12
+
+    name: Literal['sunspot'] = 'sunspot'  # type: ignore[assignment]
+    label: str = 'htex'
+
+    num_nodes: int = 1
+    """Number of nodes to request"""
+    account: str
+    """The account to charge compute to."""
+    queue: str
+    """Which queue to submit jobs to, will usually be prod."""
+    walltime: str
+    """Maximum job time."""
+    retries: int = 0
+    """Number of retries upon failure."""
+
+    def get_config(self, run_dir: PathLike) -> Config:
+        """Create a Parsl configuration for running on Sunspot."""
+        accel_ids = [
+            f"{gid}.{tid}"
+            for gid in range(6)
+            for tid in range(2)
+        ]
+        return Config(
+            executors=[
+                HighThroughputExecutor(
+                    available_accelerators=accel_ids,  # Ensures one worker per accelerator
+                    cpu_affinity="block",  # Assigns cpus in sequential order
+                    prefetch_capacity=0,
+                    max_workers=12,
+                    cores_per_worker=16,
+                    provider=PBSProProvider(
+                        launcher=MpiExecLauncher(
+                            bind_cmd="--cpu-bind",
+                            overrides="--depth=208 --ppn 1"
+                        ),  # Ensures 1 manger per node and allows it to divide work among all 208 threads
+                        worker_init="""
+export HTTP_PROXY=http://proxy.alcf.anl.gov:3128
+export HTTPS_PROXY=http://proxy.alcf.anl.gov:3128
+export http_proxy=http://proxy.alcf.anl.gov:3128
+export https_proxy=http://proxy.alcf.anl.gov:3128
+git config --global http.proxy http://proxy.alcf.anl.gov:3128
+module use /soft/modulefiles/
+module load frameworks/2023.12.15.001
+source /lus/gila/projects/candle_aesp_CNDA/hippekp/venvs/pdfwf/bin/activate""",
+                        nodes_per_block=self.num_nodes,
+                        account=self.account,
+                        queue=self.queue,
+                        walltime=self.walltime,
+
+                    ),
+                ),
+            ],
+            run_dir=str(run_dir),
+            checkpoint_mode='task_exit',
+            retries=self.retries,
+            app_cache=True,
+        )
+
+
 ComputeSettingsTypes = Union[
     LocalSettings,
     WorkstationSettings,
     PolarisSettings,
+    SunspotSettings
 ]
